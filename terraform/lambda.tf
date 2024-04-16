f4bcfdfb4d36959ebda56030f814ffa8365a3aaf
luke-h1 <luke.howsam@yahoo.com>
2024-04-16 18:30:28 +0100

    chore(lambda): reduce cw retention to 3 days

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -59,5 +59,5 @@ resource "aws_lambda_function" "lambda" {
 
 resource "aws_cloudwatch_log_group" "lambda_logs" {
   name              = "/aws/lambda/${aws_lambda_function.lambda.function_name}"
-  retention_in_days = 7
+  retention_in_days = 3
 }
