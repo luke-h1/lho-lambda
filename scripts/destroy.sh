552fd25e09efeac0b8210639be41c15795e78438
luke-h1 <luke.howsam@yahoo.com>
2024-04-17 14:08:21 +0100

    chore(lambda): release

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ "$1" == "staging" ]; then
-  terraform workspace select staging
+  # terraform workspace select staging
   cd terraform
   terraform destroy -var-file="envs/staging.tfvars" -auto-approve
 elif [ "$1" == "live" ]; then
-  terraform workspace select live
+  # terraform workspace select live
   cd terraform
   terraform destroy -var-file="envs/live.tfvars" -auto-approve
 else
