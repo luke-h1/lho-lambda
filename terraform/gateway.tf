a230b0cce1df668984f292403a00d6367d215104
luke-h1 <luke.howsam@yahoo.com>
2024-04-17 13:05:13 +0100

    chore(infrastructure): disable cw

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -36,22 +36,22 @@ resource "aws_apigatewayv2_stage" "lambda" {
     throttling_rate_limit    = 100
     detailed_metrics_enabled = true
   }
-  access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.api_gw.arn
-    format = jsonencode({
-      requestId               = "$context.requestId"
-      sourceIp                = "$context.identity.sourceIp"
-      requestTime             = "$context.requestTime"
-      protocol                = "$context.protocol"
-      httpMethod              = "$context.httpMethod"
-      resourcePath            = "$context.resourcePath"
-      routeKey                = "$context.routeKey"
-      status                  = "$context.status"
-      responseLength          = "$context.responseLength"
-      integrationErrorMessage = "$context.integrationErrorMessage"
-      }
-    )
-  }
+  # access_log_settings {
+  #   destination_arn = aws_cloudwatch_log_group.api_gw.arn
+  #   format = jsonencode({
+  #     requestId               = "$context.requestId"
+  #     sourceIp                = "$context.identity.sourceIp"
+  #     requestTime             = "$context.requestTime"
+  #     protocol                = "$context.protocol"
+  #     httpMethod              = "$context.httpMethod"
+  #     resourcePath            = "$context.resourcePath"
+  #     routeKey                = "$context.routeKey"
+  #     status                  = "$context.status"
+  #     responseLength          = "$context.responseLength"
+  #     integrationErrorMessage = "$context.integrationErrorMessage"
+  #     }
+  #   )
+  # }
 }
 
 resource "aws_apigatewayv2_integration" "lambda" {
@@ -95,10 +95,10 @@ resource "aws_apigatewayv2_route" "lambda_route_now_playing" {
 ##############################################################################
 
 
-resource "aws_cloudwatch_log_group" "api_gw" {
-  name              = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
-  retention_in_days = 3
-}
+# resource "aws_cloudwatch_log_group" "api_gw" {
+#   name              = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+#   retention_in_days = 3
+# }
 
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowExecutionFromAPIGateway"
