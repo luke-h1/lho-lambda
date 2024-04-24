7f843abe97a6f5fb40d859c5f42e783f523868cb
luke-h1 <luke.howsam@yahoo.com>
2024-04-24 20:50:47 +0100

    fix(lambda): fix version endpoint

--- apps/lambda/src/handlers/version/index.ts
+++ apps/lambda/src/handlers/version/index.ts
@@ -1,6 +1,10 @@
 import pkg from '../../../package.json';
 
 const versionHandler = async () => {
-  return JSON.stringify({ version: pkg.version });
+  return JSON.stringify({
+    version: pkg.version,
+    deployedBy: process.env.DEPLOYED_BY,
+    deployedAt: process.env.DEPLOYED_AT,
+  });
 };
 export default versionHandler;
