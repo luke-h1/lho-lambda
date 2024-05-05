66be49324a8450a48e28dc2dd0cf46068d87eb95
luke-h1 <luke.howsam@yahoo.com>
2024-05-05 11:21:12 +0100

    refactor(infrastructure): test

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,10 @@
-output "function_name" {
-  description = "value of the lambda function name"
-  value       = aws_lambda_function.lambda.function_name
-}
+# output "function_name" {
+#   description = "value of the lambda function name"
+#   value       = aws_lambda_function.lambda.function_name
+# }
 
-output "base_url" {
-  description = "Base URL of the API Gateway stage"
-  # value = module.api_gw.apigatewayv2_api_api_endpoint
-  value = aws_apigatewayv2_stage.lambda.invoke_url
-}
+# output "base_url" {
+#   description = "Base URL of the API Gateway stage"
+#   # value = module.api_gw.apigatewayv2_api_api_endpoint
+#   value = aws_apigatewayv2_stage.lambda.invoke_url
+# }
