0cc344d16802d1714e92ce1616d4829299a777bb
luke-h1 <luke.howsam@yahoo.com>
2024-05-05 15:07:35 +0100

    feat(infrastructure): test

--- apps/lambda/src/index.ts
+++ apps/lambda/src/index.ts
@@ -10,8 +10,7 @@ export const handler: Handler = async (
   event: APIGatewayProxyEvent,
   context: Context,
 ) => {
-  const path = event.path.split('/')[3] as unknown as string;
-
+  const { path } = event;
   // AWSXRay.enableAutomaticMode();
 
   console.log('path is', path);
