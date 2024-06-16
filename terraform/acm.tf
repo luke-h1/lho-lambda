76b68b0f1a181c32219d2308421e2338f47ce45c
luke-h1 <luke.howsam@yahoo.com>
2024-06-16 16:33:34 +0100

    feat(lambda): reduce certs

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -4,18 +4,6 @@ locals {
   domain_name = var.env == "live" ? "nowplaying.${var.root_domain}" : "nowplaying-staging.${var.root_domain}"
 }
 
-resource "aws_acm_certificate" "root_domain" {
-  provider          = aws.us-east-1
-  domain_name       = local.domain_name
-  validation_method = "DNS"
-  lifecycle {
-    create_before_destroy = true
-
-    # only set to false because we might need to do a full teardown
-    prevent_destroy = false
-  }
-}
-
 data "aws_route53_zone" "domain" {
   private_zone = false
   zone_id      = var.zone_id
