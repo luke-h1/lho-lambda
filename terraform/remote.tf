0bcd9a3e9c270877c300fd5a7b6629858c22bfde
luke-h1 <luke.howsam@yahoo.com>
2024-09-22 19:40:11 +0100

    fix(infrastructure): prevent concurrent terraform deploys

--- terraform/remote.tf
+++ terraform/remote.tf
@@ -2,8 +2,10 @@ data "terraform_remote_state" "vpc" {
   backend   = "s3"
   workspace = var.env
   config = {
-    bucket = var.env == "live" ? "nowplaying-live-terraform-state" : "nowplaying-staging-terraform-state"
-    key    = var.env == "live" ? "vpc/live.tfstate" : "vpc/staging.tfstate"
-    region = "eu-west-2"
+    bucket         = var.env == "live" ? "nowplaying-live-terraform-state" : "nowplaying-staging-terraform-state"
+    key            = var.env == "live" ? "vpc/live.tfstate" : "vpc/staging.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = var.env == "live" ? "nowplaying-live-terraform-state-lock" : "nowplaying-staging-terraform-state-lock"
+    encrypt        = true
   }
 }
