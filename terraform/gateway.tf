4abf5f3a2f7915e8b3dd34ab070eb6462f1e1eec
luke-h1 <luke.howsam@yahoo.com>
2025-08-02 15:27:49 +0100

    chore(alerts): simulate downtime

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -94,20 +94,19 @@ resource "aws_apigatewayv2_integration" "lambda" {
 # ROUTES 
 ##############################################################################
 resource "aws_apigatewayv2_route" "lambda_route_health" {
-  api_id         = aws_apigatewayv2_api.lambda.id
-  target         = "integrations/${aws_apigatewayv2_integration.lambda.id}"
-  route_key      = "GET /api/health"
-  operation_name = "health"
+  api_id             = aws_apigatewayv2_api.lambda.id
+  target             = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+  route_key          = "GET /api/health"
+  operation_name     = "health"
+  authorizer_id      = aws_apigatewayv2_authorizer.api_key.id
+  authorization_type = "CUSTOM"
 }
 
 resource "aws_apigatewayv2_route" "lambda_route_health_head" {
-  api_id             = aws_apigatewayv2_api.lambda.id
-  target             = "integrations/${aws_apigatewayv2_integration.lambda.id}"
-  route_key          = "HEAD /api/health"
-  operation_name     = "head health"
-  authorizer_id      = aws_apigatewayv2_authorizer.api_key.id
-  authorization_type = "CUSTOM"
-
+  api_id         = aws_apigatewayv2_api.lambda.id
+  target         = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+  route_key      = "HEAD /api/health"
+  operation_name = "head health"
 }
 
 resource "aws_apigatewayv2_route" "lambda_route_version" {
